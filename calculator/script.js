86cce367930229dc4d2533778ca35585f4d7b4fb
zuzu-18 <136903292+zuzu-18@users.noreply.github.com>
2024-07-25 20:59:18 +0500

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,32 @@
+let screen = document.getElementById("display");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    if (buttonText == "X") {
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }else if (buttonText == "c"){
+      screenValue ="";
+      screen.value = screenValue;
+    } 
+    else if (buttonText == "del") {
+      screenValue = screenValue.slice(0,-1);
+      screen.value = screenValue;
+    } else if (buttonText == "=") {
+      var result;
+      try {
+        result = eval(screenValue);
+      } catch (error) {
+        result = "Expression error";
+      }
+      screen.value = result;
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
+}
+
